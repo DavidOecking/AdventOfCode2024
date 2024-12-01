4a86c9d0aaf61cc4ffa14b75291f902e60472b2a
david <david.oecking@tu-dortmund.de>
2024-12-01 11:09:24 +0100

    Puzzle 2 implementation

--- Advent1/advent1.cpp
+++ Advent1/advent1.cpp
@@ -18,6 +18,7 @@ void insertSorted(std::vector<int>& list, int value){
 int main(int argc, char** argv){
 
     int difference = 0;
+    int similiarity = 0;
     std::vector<int> left, right;
 
     std::ifstream inputFile("input");
@@ -34,12 +35,26 @@ int main(int argc, char** argv){
         insertSorted(left, first);
         insertSorted(right, second);
     }
+
     for(size_t i = 0; i<left.size(); ++i){
         difference += std::abs(left[i]-right[i]);
     }
-    std::cout << "Difference: ";
-    std::cout << difference;
-    std::cout << std::endl; 
+    std::cout << "Difference: " << difference << std::endl;
+
+    size_t right_index = 0;
+    for(size_t left_index = 0; left_index<left.size(); ++left_index){
+        while(left[left_index]>right[right_index]){
+            right_index++;
+        }
+        int count = 0;
+        while(left[left_index]==right[right_index]){
+            count++;
+            right_index++;
+        }
+        similiarity += count * left[left_index];
+    }
+    std::cout << "Similarity: " << similiarity << std::endl;
+
     return 0;
 }
 
