1f80145b5b2658ae57db58c724a0ed4ee530511a
David Oecking <david.oecking@gmx.de>
2024-12-03 09:23:46 +0100

    Solution Advent 3 Puzzle 1

--- Advent3/advent3.cpp
+++ Advent3/advent3.cpp
@@ -3,23 +3,69 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <cctype>
 
 int main(int argc, char** argv){
 
-    std::string comparisonString = "mult(*,*)";
-    int result = 1;
+    std::string comparisonString = "mul(,)";
+    std::string doString = "do()";
+    std::string dontString = "don't()";
+    int result = 0;
     std::vector<std::vector<int>> safetyReports;
     std::ifstream inputFile("input");
     std::string line;
+
     while (std::getline(inputFile,line)) {
+
         std::stringstream currentLineStream(line);
         std::string currentWord;
-        int currentIndex = 0;
+        bool reset = false;
+        bool doMult = true;
+        int index = 0;
+        int numberLength = 0;
+        std::string firstMult = "";
+        std::string secondMult = "";
         char ch;
-        while (ss.get(ch)) { 
-            std::cout << ch << std::endl;
+
+        while (currentLineStream.get(ch)) { 
+
+            if(std::isdigit(ch)){
+                if(index == 4 || index == 5){
+                    if(numberLength<3){
+                        if(index == 4){
+                           firstMult += ch;
+                        }
+                        if(index == 5){
+                            secondMult += ch;
+                        }
+                        numberLength++;
+                    }else{
+                        reset = true;
+                    }
+                }else{
+                    reset = true;
+                }
+            }else if(ch == comparisonString[index]){
+                if(index == comparisonString.size()-1){
+                    result += std::stoi(firstMult) * std::stoi(secondMult);
+                    reset = true;
+                }else{
+                    index++;
+                    numberLength = 0;
+                }
+            }else{
+                reset = true;
+            }
+            if(reset){
+                index = 0;
+                firstMult = "";
+                secondMult = "";
+                numberLength = 0;
+                reset = false;
+            }
         }   
     }
+    std::cout << "The product is: " << result << std::endl;
 
     return 0;
 }
